60675791060f76cb3c2efaa0d4f2c17203a8d363
Colton Ogden <coltonoscopy@gmail.com>
2017-10-21 20:12:32 -0400

    first commit

---
+++ starfox/main.lua
@@ -0,0 +1,11 @@
+--[[
+    GD50 2018
+    Pong Remake
+
+    Author: Colton Ogden
+    cogden@cs50.harvard.edu
+
+    Originally programmed by Atari in 1972. Features two
+    paddles, controlled by players, with the goal of getting
+    the ball past your opponent's edge. First to 10 points wins.
+]]
